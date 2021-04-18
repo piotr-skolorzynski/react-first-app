04d382f1f1fb3dd097160ab606e9e9e1de20f292
Piotr Skolorzynski <p.skolorzynski@gmail.com>
2021-04-18 22:13:24 +0200

    app.js and index.js files modified. ninjas.js file created to show example how to nest component inside other component

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Ninjas from './ninjas';
 
 class App extends Component {
   render() {
-    return (
+    return(
     <div className="App">
       <h1>My first React app!</h1>
       <p>Hello and Welcome ;)</p>
+      <Ninjas />
     </div>
     );
   }
---
+++ first-react-app/src/ninjas.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Ninjas extends Component {
+    render() {
+        return (
+        <div className="ninja">
+            <div>Name: Piotr</div>
+            <div>Age: 39</div>
+            <div>Belt: Blue</div>
+        </div>
+        );
+    }
+}
+
+export default Ninjas
