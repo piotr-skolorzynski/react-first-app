179d2e0967eb88ac8d844ab898b8cb33d3d1ed7d
Piotr Skolorzynski <p.skolorzynski@gmail.com>
2021-04-19 22:01:15 +0200

    App.js and ninjas.js files modified to show how output lists

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import Ninjas from './ninjas';
 
 class App extends Component {
+  state = {
+    ninjas : [
+      { name: 'Mateusz', age: 33, belt: 'black', id: 1 },
+      { name:'Piotr', age: 39, belt: 'blue', id: 2 },
+      { name: 'Magda', age: 27, belt: 'braun', id: 3 }
+    ]
+  }
   render() {
     return(
     <div className="App">
       <h1>My first React app!</h1>
       <p>Hello and Welcome ;)</p>
-      <Ninjas name="Mateusz" age="33" belt="black"/>
-      <Ninjas name="Piotr" age="39" belt="blue"/>
+      <Ninjas ninjas={this.state.ninjas}/>
     </div>
     );
   }
